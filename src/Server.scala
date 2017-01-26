89ff2e325249b323500607ab92614accdf4dcbbb
Þorgeir <thorgeir@radiantgames.is>
2017-01-26 18:02:29 +0000

    Very basic stuff, no data management yet

--- src/Server.scala
+++ src/Server.scala
@@ -1,11 +1,36 @@
 /**
   * Created by centos on 1/26/17.
   */
-class Server(id: Int, n: Int) {
-  var copys:Array[Int] = new Array[Int](n);
+  
+// Id = hash id for this server.
+// n = how many clones 
+class Server(id: Int, n: Int, circleSize:Int) {
+  private var writeCount : Int;
+  private var cloneServerIds : Array[Int] = new Array[Int](n);
+  private var data : collection.mutable.Map[Int, String];
 
-  def connect(): Unit = {
+  var nextServer : Server;
+  var prevServer : Server;
+  
+  def get(key: Int) : String { }
+  def put(key: Int, data : String) { }
 
+  def findSuccessor(key : Int):Server {
+    if (id > nextServer.id){ 
+      // the bridge.
+      if (key > id || key < nextServer.id ) return nextServer;
+    }
+    if (id < key && nextServer.id > key) return nextServer;
+
+    return nextServer.findSuccessor(key);
   }
 
+  def connect(network : Server): Unit = { 
+    // Here we need to split the data space between this Server and the existingServer.
+    nextServer = network.findSuccessor(id);
+    prevServer = nextServer.prevServer;
+
+    prevServer.nextServer = this;
+    nextServer.prevServer = this;
+  }
 }
