a18f2cf9e8cfb4c99d7f9b3e4119bc94f7dc8680
Þorgeir <thorgeir@radiantgames.is>
2017-01-26 18:37:29 +0000

    Minimal functunality now up and still have to debug

--- src/Main.scala
+++ src/Main.scala
@@ -2,15 +2,29 @@
   * Created by centos on 1/26/17.
   */
 
+import scala.util
+
 object Main {
+  var s = 10;
+  var e = 10000;
+  var n = 3;
+  var w = 1000000;
+  var i = 5;
+  var sm = 30;
 
   def main(args: Array[String]): Unit = {
-    var s = 10;
-    var e = 10000;
-    var n = 3;
-    var w = 1000000;
-    var i = 5;
-    var sm = 30;
+    parsePara(args);
+
+    var rnd = new Random();
+    var network = new Server(rnd.nextInt(e+1),n);
+    for (var x <- 0..s)
+      network.connect(new Server(rnd.nextInt(e+1),n));
+
+    for (var x <- 0..w)
+      network.put(rnd.nextInt(e+1),"lol"+x);
+  }
+
+  def parsePara(args: Array[String]):Unit{
     for (arg <- args) {
       if (arg == "-s"){
         s = args.tail.head.toInt;
@@ -31,6 +45,5 @@ object Main {
         sm = args.tail.head.toInt;
       }
     }
-
   }
 }
--- src/Server.scala
+++ src/Server.scala
@@ -4,25 +4,54 @@
   
 // Id = hash id for this server.
 // n = how many clones 
-class Server(id: Int, n: Int, circleSize:Int) {
+class Server(id: Int, n: Int) {
   private var writeCount : Int;
-  private var cloneServerIds : Array[Int] = new Array[Int](n);
-  private var data : collection.mutable.Map[Int, String];
+  private var datamap : collection.mutable.Map[Int, String];
 
-  var nextServer : Server;
-  var prevServer : Server;
+  var nextServer : Server = this;
+  var prevServer : Server = this;
   
-  def get(key: Int) : String { }
-  def put(key: Int, data : String) { }
-
-  def findSuccessor(key : Int):Server {
+  def get(key: Int) : String { 
+    if (belongsToMyGroup(key)) return data[key]; 
+    else return nextServer.get(key);
+  }
+  def put(key: Int, data : String) { 
+    if (belongsToMe(key))
+    {
+      var cur = this;
+      for (var x <- 0 to n){
+        cur.writeData(key,data);
+        cur = cur.nextServer;
+      }
+    }
+    else nextServer.put(key,data);
+  }
+  def writeData(key:Int, data:String){
+    writeCount++;
+    datamap[key] = data;
+  }
+  
+  def findSuccessor(key : Int) : Server {
+    if (belongs(key)) return nextServer;
+    else return nextServer.findSuccessor(key);
+  }
+  def belongsToMe(key : Int) : Boolean {
+    if (key == id) return true;
     if (id > nextServer.id){ 
       // the bridge.
-      if (key > id || key < nextServer.id ) return nextServer;
+      if (key > id || key < nextServer.id ) return true;
     }
-    if (id < key && nextServer.id > key) return nextServer;
+    if (id < key && nextServer.id > key) return true;
 
-    return nextServer.findSuccessor(key);
+    return false;
+  }
+  def belongsToMyGroup(key : Int) : Boolean {
+    if (belongsToMe(key)) return true;
+    var cur = nextServer;
+    for (var x <- 0 to n){
+      if (cur.belongsToMe(key)) return true;
+      cur = cur.nextServer;
+    }
   }
 
   def connect(network : Server): Unit = { 
